e23e8e7cc22ef1970ead0f61977ee06ae30cd34f
AriZume <128248493+AriZume@users.noreply.github.com>
2023-11-04 20:53:00 +0200

    UI layout w/o post fuction

--- project/app/src/main/java/com/example/geosnap/MainActivity.java
+++ project/app/src/main/java/com/example/geosnap/MainActivity.java
@@ -1,15 +1,49 @@
 package com.example.geosnap;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.widget.Button;
 
+import com.example.geosnap.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if(id == R.id.home){
+                replaceFragment(new HomeFragment());
+            } else if (id == R.id.search) {
+                replaceFragment(new SearchFragment());
+            }
+            return false;
+        });
+
+        Button postBtn = findViewById(R.id.postBtn);
+        postBtn.setOnClickListener(v -> openPostActivity());
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    public void openPostActivity(){
+        Intent intent = new Intent(MainActivity.this, PostActivity.class);
+        startActivity(intent);
     }
 }
