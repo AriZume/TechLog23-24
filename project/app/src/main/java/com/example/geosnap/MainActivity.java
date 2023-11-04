6e016390d107778936dc500b1057402a9c3512dd
Helen1Z <helenz1@windowslive.com>
2023-11-04 21:13:43 +0200

    database preparation

--- project/app/src/main/java/com/example/geosnap/MainActivity.java
+++ project/app/src/main/java/com/example/geosnap/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.geosnap.databases.PicturesDB;
+
 public class MainActivity extends AppCompatActivity {
+    PicturesDB db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //if () {
+         db.initPic(this);
+        //}
     }
 }
