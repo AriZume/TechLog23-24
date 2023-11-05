55800119996a88f60a636a8f41397d285718fcc8
AriZume <128248493+AriZume@users.noreply.github.com>
2023-11-05 15:23:57 +0200

    Revert "Merge pull request #11 from AriZume/databases"
    This reverts commit e5ced3fbb985906a3c5a65d0ec569f52373caad8, reversing
    changes made to 9fb5fe2fffe5070207abb57ed8d40171a2f7041e.

--- project/app/src/main/java/com/example/geosnap/MainActivity.java
+++ project/app/src/main/java/com/example/geosnap/MainActivity.java
@@ -1,72 +1,15 @@
 package com.example.geosnap;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import com.example.geosnap.databases.DatabaseAdapter;
-import com.example.geosnap.databases.DatabaseData;
-import com.example.geosnap.databases.UploadActivity;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    FloatingActionButton fab;
-    RecyclerView recyclerView;
-    ArrayList<DatabaseData> dataList;
-    DatabaseAdapter adapter;
-    private final DatabaseReference dbRef= FirebaseDatabase
-            .getInstance().getReference("pictures");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        fab= findViewById(R.id.fab);
-        recyclerView= findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        dataList= new ArrayList<>();
-        adapter= new DatabaseAdapter(dataList, this);
-        recyclerView.setAdapter(adapter);
-        dbRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot shot: snapshot.getChildren()){
-                    DatabaseData data= shot.getValue(DatabaseData.class);
-                    dataList.add(data);
-                }
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, UploadActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
     }
 }
