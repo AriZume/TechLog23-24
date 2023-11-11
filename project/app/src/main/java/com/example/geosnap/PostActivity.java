6acccd7a0bde2fa67c8fc11bc9350f85024cef8c
Tziouve <steliostz2002@gmail.com>
2023-11-11 19:09:28 +0200

    Bottom sheet

--- project/app/src/main/java/com/example/geosnap/PostActivity.java
+++ project/app/src/main/java/com/example/geosnap/PostActivity.java
@@ -5,9 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.widget.Button;
+
+import android.app.Dialog;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class PostActivity extends AppCompatActivity {
 
+    Button bottomSheet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +27,27 @@ public class PostActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Upload...");
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FF252526")));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        bottomSheet = findViewById(R.id.addTagBtn);
+        bottomSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
     }
+
+    private void showDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottom_sheet_layout);
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+    }
+
+
 }
