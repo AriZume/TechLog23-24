8784d5a21ab124b8fb7da9fa097d5a2d7fda0181
alk-an <147655333+alk-an@users.noreply.github.com>
2023-11-11 21:18:18 +0200

    den

--- project/app/src/main/java/com/example/geosnap/fragments/HomeFragment.java
+++ project/app/src/main/java/com/example/geosnap/fragments/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.geosnap.fragments;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -8,27 +9,28 @@ import android.os.Bundle;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.geosnap.MainActivity;
-import com.example.geosnap.Manifest;
+//import com.example.geosnap.Manifest;
 import com.example.geosnap.R;
 
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 public class HomeFragment extends Fragment {
+    Location location = null;
 
         @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
@@ -46,13 +48,19 @@ public class HomeFragment extends Fragment {
             public void onMapReady(GoogleMap googleMap) {
                 // When map is loaded
 
-                UserLocation userobj = new UserLocation();
+
+
+                //UserLocation userobj = new UserLocation();
                 //Location loc = userobj.getLocation();
-                userobj.getLastLocation();
-                double latitude = userobj.getLat();//loc.getLatitude();
-                double longitude = userobj.getLon();//loc.getLongitude();
+                //userobj.getLastLocation();
+                //double latitude = userobj.getLat();//loc.getLatitude();
+                //double longitude = userobj.getLon();//loc.getLongitude();
+
+
+
+
                 // Create a LatLng object with the initial position
-                LatLng initialPosition = new LatLng(latitude,longitude);//41.0749, 23.5555);//latitude,longitude);
+                LatLng initialPosition = new LatLng(location.getLatitude(),location.getLongitude());//41.0749, 23.5555);//latitude,longitude);
                 // Initialize marker options
                 MarkerOptions markerOptions = new MarkerOptions();
                 // Set position of marker
@@ -90,6 +98,7 @@ public class HomeFragment extends Fragment {
         // Return view
         return view;
     }
+
 
 }
 
--- project/app/src/main/java/com/example/geosnap/fragments/UserLocation.java
+++ project/app/src/main/java/com/example/geosnap/fragments/UserLocation.java
@@ -1,164 +1,76 @@
 package com.example.geosnap.fragments;
-import android.Manifest;
+
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Looper;
-import android.provider.Settings;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
+import androidx.core.content.ContextCompat;
 
+import com.example.geosnap.Manifest;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-public class UserLocation extends Activity {//implements OnMapReadyCallback{
+import com.google.android.gms.tasks.OnSuccessListener;
 
-    // initializing
-    // FusedLocationProviderClient
-    // object
-    FusedLocationProviderClient mFusedLocationClient;
+import java.util.Locale;
 
-    // Initializing other items
-    // from layout file
-    double latitude, longitude;
-    int PERMISSION_ID = 44;
+class UserLocation {
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private final int REQUEST_CODE=100;
 
-    public double getLat() {
-       // getLastLocation();
-        return latitude;
-    }
+    double latitude , longitude
 
-    public double getLon() {
-       // getLastLocation();
-        return longitude;
-    }
-
-    @SuppressLint("MissingPermission")
-    public void getLastLocation() {
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        // check if permissions are given
-        if (checkPermissions()) {
-
-            // check if location is enabled
-            if (isLocationEnabled()) {
-
-                // getting last
-                // location from
-                // FusedLocationClient
-                // object
-                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        Location location = task.getResult();
-                        if (location == null) {
-                            requestNewLocationData();
-                        } else {
-                            latitude=location.getLatitude();
-                            longitude=location.getLongitude();
-                        }
-                    }
-                });
-            } else {
-                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        } else {
-            // if permissions aren't available,
-            // request for permissions
-            requestPermissions();
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private void requestNewLocationData() {
-
-        // Initializing LocationRequest
-        // object with appropriate methods
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
-        mLocationRequest.setInterval(5);
-        mLocationRequest.setFastestInterval(0);
-        mLocationRequest.setNumUpdates(1);
-
-        // setting LocationRequest
-        // on FusedLocationClient
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
-    }
-
-    private LocationCallback mLocationCallback = new LocationCallback() {
-
-        @Override
-        public void onLocationResult(LocationResult locationResult) {
-            Location mLastLocation = locationResult.getLastLocation();
-            latitude= mLastLocation.getLatitude();
-            longitude=mLastLocation.getLongitude();
-        }
-    };
-
-    // method to check for permissions
-    private boolean checkPermissions() {
-        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-
-        // If we want background location
-        // on Android 10.0 and higher,
-        // use:
-        // ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
-    }
-
-    // method to request for permissions
-    private void requestPermissions() {
-        ActivityCompat.requestPermissions(this, new String[]{
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
-    }
-
-    // method to check
-    // if location is enabled
-    private boolean isLocationEnabled() {
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-    }
-
-    // If everything is alright then
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == PERMISSION_ID) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLastLocation();
-            }
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (checkPermissions()) {
+    public getLocation(){
+        fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this);
+        getLocation.setOnClickListener(new View.OnClickListener(){
             getLastLocation();
+        });
+
+
+    }
+    private void getLastLocation(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            fusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+
+                            if(location != null){
+                                Geocoder geocoder= new Geocoder(UserLocation.this, Locale.getDefault());
+                                latitude.getLatitude();
+                                longitude.getLongitude();
+                            }
+                        }
+                    });
+        }else{
+
+            askPermission();
         }
     }
 
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-        super.onPointerCaptureChanged(hasCapture);
+    private void askPermission() {
+        ActivityCompat.requestPermissions(UserLocation.this, new String[]
+                {Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
     }
-
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permission , @NonNull String[] grantResults){
+        if(requestCode==REQUEST_CODE){
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLastLocation();
+            }else{
+                Toast.makeText(this, "R", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
+
 
